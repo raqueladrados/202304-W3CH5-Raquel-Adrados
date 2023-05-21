33dd04a67695cdc02a65362f29f0000b64b29a28
Raquel Adrados <radrados00@gmail.com>
2023-05-21 23:26:54 +0200

    Add header test and change the get pokemon function

--- src/components/pokemon.list.ts
+++ src/components/pokemon.list.ts
@@ -1,19 +1,9 @@
-import { Component } from './component';
-import { PokemonRepository } from '../data/api.pokemon.repository';
-import { Pokemon } from '../models/pokemon';
-
-export class PokemonList extends Component {
-  pokemon!: Pokemon[];
-  list: PokemonRepository;
-  constructor(selector: string) {
-    super(selector);
-    this.pokemon = [];
-    this.list = new PokemonRepository();
-  }
-
-  async handleLoad() {
-    this.pokemon = await this.list.getAll();
-    this.render();
-    console.log(this.element);
-  }
+export interface PokemonList {
+  count: number;
+  next: string;
+  previous: string;
+  results: {
+    name: string;
+    url: string;
+  };
 }
--- src/data/get.pokemon.ts
+++ src/data/get.pokemon.ts
@@ -2,7 +2,7 @@ import { PokemonList } from '../components/pokemon.list';
 const getPokemon = () => {
   fetch('https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20')
     .then((response) => response.json())
-    .then((pokemon) => console.log(pokemon));
+    .then((_response: PokemonList) => {});
 };
 
 getPokemon();
